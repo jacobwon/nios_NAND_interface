bfac10678b94fefc4af9c8f06707ff78be231c1f
Sorty Mmith <sortymmith@outlook.com>
2020-03-04 19:32:16 -0600

    added initial skeleton codes

---
+++ nios/my_main.c
@@ -0,0 +1,23 @@
+/*
+File: my_main.c
+Description: This file is created inside the project 'nios_nand_interface'
+.. The goal of this project is to create a computer system that interfaces with a ONFI2.2 standard
+.. .. memory system
+.. The computer system used here is DE1_SoC that runs at 100Mhz (10 ns period)
+.. connection to the NAND is made in parallel port 1 on JP1
+.. .. the address of which is at 0xff200060
+
+Author: Prawar Poudel (pp0030@uah.edu)
+		4 March 2020
+*/
+
+// let us include mandatory header file
+#include <stdio.h>
+
+// user defined header file
+#include <nios_interface_header.h>
+
+int main()
+{
+
+}
---
+++ nios/nand_interface_header.h
@@ -0,0 +1,6 @@
+#ifndef nand_interface_header_h
+#define nand_interface_header_h
+
+// put the user defined header codes here
+
+#endif
