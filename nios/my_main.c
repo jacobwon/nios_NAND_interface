66a92b12b49965b63c431449f1003bd80da40cb7
Sorty Mmith <sortymmith@outlook.com>
2020-04-02 15:10:52 -0500

    add next address to test

--- nios/my_main.c
+++ nios/my_main.c
@@ -47,8 +47,9 @@ int main()
 	// let us just read a random page for now
 	// .. 5-byte page address
 	// .. c1,c2,r1,r2,r3
-	uint8_t my_page_address[5] = {0x00,0x00,0x0,0xf7,0x00}; 
-	// uint8_t new_col[2] = {0x10,0x00};
+	uint8_t my_page_address[5] = {0x00,0x00,0x0,0xf7,0x06}; 
+	uint8_t my_page_address2[5] = {0x00,0x00,0x01,0xf7,0x06}; 
+	uint8_t new_col[2] = {50,0};
 	uint8_t data_received[8192];
 
 	printf(" * Printing array value:");
@@ -58,15 +59,38 @@ int main()
 	printf(" * Reading address (address in reverse order:)");
 	print_array(my_page_address,5);
 	read_page(my_page_address,5);
+	change_read_column(new_col);
+	get_data(data_received,100);
+	print_array(data_received,100);
+
+	printf(" * Reading address (address in reverse order:)");
+	print_array(my_page_address2,5);
+	read_page(my_page_address2,5);
+	change_read_column(new_col);
 	get_data(data_received,100);
 	print_array(data_received,100);
 
 	// let us erase the block
 	printf(" * Erasing address (address in reverse order:)");
 	print_array(my_page_address+2,3);
+	enable_erase();
 	erase_block(my_page_address+2);
+	disable_erase();
 	// now read the erased page, should be 0xffs
 	read_page(my_page_address,5);
+	change_read_column(new_col);
+	get_data(data_received,100);
+	print_array(data_received,100);
+
+	// let us erase the block
+	printf(" * Erasing address (address in reverse order:)");
+	print_array(my_page_address2+2,3);
+	enable_erase();
+	erase_block(my_page_address2+2);
+	disable_erase();
+	// now read the erased page, should be 0xffs
+	read_page(my_page_address2,5);
+	change_read_column(new_col);
 	get_data(data_received,100);
 	print_array(data_received,100);
 
@@ -80,8 +104,21 @@ int main()
 	// let us program the page
 	printf(" * Programming address (address in reverse order:)");
 	print_array(my_page_address,5);
+	enable_erase();
 	program_page(my_page_address,data_received,8192);
+	disable_erase();
 	read_page(my_page_address,5);
+	change_read_column(new_col);
+	get_data(data_received,100);
+	print_array(data_received,100);
+
+	printf(" * Programming address (address in reverse order:)");
+	print_array(my_page_address2,5);
+	enable_erase();
+	program_page(my_page_address2,data_received,8192);
+	disable_erase();
+	read_page(my_page_address2,5);
+	change_read_column(new_col);
 	get_data(data_received,100);
 	print_array(data_received,100);
 
