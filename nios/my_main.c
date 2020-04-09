269783cfbd943bea90a7fa153ff0f83347b99cf1
Sorty Mmith <sortymmith@outlook.com>
2020-04-08 21:18:39 -0500

    add code to profile erase and program ops

--- nios/my_main.c
+++ nios/my_main.c
@@ -58,9 +58,24 @@ int main()
 
 	printf(" * Reading address (address in reverse order:)");
 	print_array(my_page_address,5);
+
+#if TIMER_PROFILE
+	printf("Read Page Operation Follows\n");
+	timer_start();
+#endif
 	read_page(my_page_address,5);
+#if TIMER_PROFILE
+	PRINT_CC_TAKEN;
+#endif	
 	change_read_column(new_col);
+#if TIMER_PROFILE
+	printf("Get 100 data-bytes from a Page Operation Follows\n");
+	timer_start();
+#endif
 	get_data(data_received,100);
+#if TIMER_PROFILE
+	PRINT_CC_TAKEN;
+#endif	
 	print_array(data_received,100);
 
 	printf(" * Reading address (address in reverse order:)");
@@ -74,8 +89,16 @@ int main()
 	printf(" * Erasing address (address in reverse order:)");
 	print_array(my_page_address+2,3);
 	enable_erase();
+#if TIMER_PROFILE
+	printf("Erase Block Operation Follows\n");
+	timer_start();
+#endif
 	erase_block(my_page_address+2);
+#if TIMER_PROFILE
+	PRINT_CC_TAKEN;
+#endif	
 	disable_erase();
+
 	// now read the erased page, should be 0xffs
 	read_page(my_page_address,5);
 	change_read_column(new_col);
@@ -86,7 +109,14 @@ int main()
 	printf(" * Erasing address (address in reverse order:)");
 	print_array(my_page_address2+2,3);
 	enable_erase();
+#if TIMER_PROFILE
+	printf("Erase Block Operation Follows\n");
+	timer_start();
+#endif
 	erase_block(my_page_address2+2);
+#if TIMER_PROFILE
+	PRINT_CC_TAKEN;
+#endif	
 	disable_erase();
 	// now read the erased page, should be 0xffs
 	read_page(my_page_address2,5);
@@ -105,7 +135,14 @@ int main()
 	printf(" * Programming address (address in reverse order:)");
 	print_array(my_page_address,5);
 	enable_erase();
+#if TIMER_PROFILE
+	printf("Program Page Operation Follows\n");
+	timer_start();
+#endif
 	program_page(my_page_address,data_received,8192);
+#if TIMER_PROFILE
+	PRINT_CC_TAKEN;
+#endif	
 	disable_erase();
 	read_page(my_page_address,5);
 	change_read_column(new_col);
@@ -115,7 +152,14 @@ int main()
 	printf(" * Programming address (address in reverse order:)");
 	print_array(my_page_address2,5);
 	enable_erase();
+#if TIMER_PROFILE
+	printf("Program Page Operation Follows\n");
+	timer_start();
+#endif
 	program_page(my_page_address2,data_received,8192);
+#if TIMER_PROFILE
+	PRINT_CC_TAKEN;
+#endif	
 	disable_erase();
 	read_page(my_page_address2,5);
 	change_read_column(new_col);
